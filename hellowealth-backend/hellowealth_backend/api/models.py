0aa537c14abe7b71df71d4aa2292a1c50042d3bd
Jim Le <taki.lnt@gmail.com>
2021-03-14 00:44:10 -0800

    Add user and transaction model for backend APIs

--- hellowealth-backend/hellowealth_backend/api/models.py
+++ hellowealth-backend/hellowealth_backend/api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
+from . import enums
 
 # Create your models here.
+class TradeTransaction(models.Model):
+    transDate = models.DateTimeField(auto_now_add=True)
+    transContent = models.CharField(max_length=255)
+    transType = models.CharField(max_length=25,
+        db_index=True,
+        choices=enums.TRANS_TYPES,
+        default=enums.TRANS_TYPE_PLUS)
+
+    amount = models.IntegerField(blank=True,
+        null=True,
+        default=0,
+        db_index=True,
+        help_text="Transaction's amount")
+
+    balance = models.IntegerField(blank=True,
+        null=True,
+        default=0,
+        db_index=True,
+        help_text="The current balance when transaction was posted")
+
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class UserPorfolio(models.Model):
+    alerts = JSONField()
+    assetEquities = JSONField()
+    watchedEquities = JSONField()
+    accountBalance = models.IntegerField(blank=True,
+        null=True,
+        default=0,
+        db_index=True,
+        help_text="User account balance")
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
