f59dd7ba61030f0afff09aa012af7f930cdf11ea
Emma Tran <emmatran9894@gmail.com>
2021-04-10 15:23:33 -0700

    Finish stock prediction processing

---
+++ hellowealth-stockPrediction/get_data_from_yahoo.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+import pymongo
+
+
+stock='AC'
+to_date = '2021-04-02'
+from_date = '2016-01-01'
+
+cluster_name = "hellowealth.wnyrs"
+mongodb_user = "test"
+mongodb_pass = "1111"
+db = 'stock_db'
+collection_name = stock.lower()
+
+stock_df = yf.download(stock, start=from_date, end=to_date, progress=False)
+stock_df = stock_df.drop(columns=['Open', 'High', 'Low', 'Adj Close', 'Volume'])
+stock_df = stock_df.reset_index()
+print(stock_df.tail())
+
+
+client = pymongo.MongoClient(
+    "mongodb+srv://{}:{}@{}.mongodb.net/{}?retryWrites=true&w=majority".format(mongodb_user, mongodb_pass, cluster_name,
+                                                                               db))
+db = client.stock_db
+collection = db[collection_name]
+collection.drop()
+
+for index, row in stock_df.iterrows():
+    print(row['Date'])
+    # break
+    my_dict = {"date": row['Date'], "Close": row['Close']}
+    x = collection.insert_one(my_dict)
